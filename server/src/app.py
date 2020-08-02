8e0813466b104b917e33ab5440df1526ba04cf60
Siddhanth Unnithan <sidunnithan@gmail.com>
2020-08-02 14:38:27 -0400

    api placeholders and gitignore

--- server/src/app.py
+++ server/src/app.py
@@ -10,6 +10,13 @@ def hello_world():
 def test_server():
     return "Smart-snippet server running"
 
+@app.route("/submit_query", methods=['POST'])
+def submit_query():
+    return "query submitted"
+
+@app.route("/submit_reviewed_snippet", methods=['POST'])
+def submit_reviewed_snippet():
+    return "reviewed snippet submitted"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-    
