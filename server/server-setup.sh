d8a2fca6905b04e4ab68d07c540d8e06ceb9be31
Siddhanth Unnithan <sidunnithan@gmail.com>
2020-08-02 22:12:48 -0400

    gitignore changes and fixed server setup script

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -1,4 +1,4 @@
 # script to start server docker container
 # usage: bash server-setup.sh
 
-docker run -d -it --name=server -p 5000:5000 -v $PWD:/App --net=ss-network --env=variables.evn server-image
+docker run -d -it --name=server -p 5000:5000 -v $PWD:/App --net=ss-network --env-file variables.env server-image
