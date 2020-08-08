e3f0390f709cd2a3532821066c2834414f8f49bb
Siddhanth Unnithan <sidunnithan@gmail.com>
2020-08-08 01:51:22 -0400

    Added support for submitting user reviewed snippets

--- server/src/utils.py
+++ server/src/utils.py
@@ -3,7 +3,9 @@ import os
 import psycopg2 as pg
 import openai
 
-from default_prompt import default_prompt
+from default_prompt import default_prompt as default_prompt
+
+# data_type_control_flow_prompt
 
 # HELPER METHODS
 def get_openai_key():
@@ -54,6 +56,22 @@ class Cursor:
         """
         self.conn.commit()
 
+    def execute(self, query):
+        """
+            Public method to execute query.
+
+            Parameters
+            ----------
+            query : {str}
+                PostgreSQL compatible query string 
+        """
+        try:
+            self.crs.execute(query)
+        except:
+            return False
+
+        return True
+
     def fetch_dict(self, query):
         """
             Public method to get query result as a list of dictionaries.
@@ -63,11 +81,14 @@ class Cursor:
             query : {str}
                 PostgreSQL compatible query string.
         """
-        self.crs.execute(query)
+        try:
+            self.crs.execute(query)
 
-        cols = self._get_cols()
+            cols = self._get_cols()
 
-        result = self.crs.fetchall()
+            result = self.crs.fetchall()
+        except:
+            return []
 
         return [dict(zip(cols, res)) for res in result]
 
@@ -98,7 +119,7 @@ class QueryPipeline:
         self.engine = \
             kwargs['engine'] if 'engine' in kwargs else 'davinci-msft'
         self.temperature = \
-            kwargs['temperature'] if 'temperature' in kwargs else 0.5
+            kwargs['temperature'] if 'temperature' in kwargs else 0.3
         self.num_tokens = \
             kwargs['num_tokens'] if 'num_tokens' in kwargs else 100
         self.stop_seq = \
@@ -129,6 +150,7 @@ class QueryPipeline:
         """
 
         self.prompt = "Once upon a time"
+        self.current_user_prompt = self.prompt
 
     def build_prompt(self, input):
         """
@@ -154,11 +176,12 @@ class QueryPipeline:
 
         text = input['text']
 
+        # keep text prompt for eventual database store
+        self.current_user_prompt = text
+
         added_prompt = f"\nQuery: {text}"
-        added_prompt += "\nResult:"
         added_prompt += "\n"
         
-
         # query for existing examples from user
         sql_query = f"""
             SELECT snippet_id FROM user_snippet WHERE user_id = '{user_id}'
@@ -173,21 +196,21 @@ class QueryPipeline:
         
         # use existing examples and further build the prompt
         sql_query = f"""
-            SELECT snippet.query, snippet.generated_snippet
-            FROM snippet
-            INNER JOIN on snippet.snippet_id = user_snippet.snippet_id
-            WHERE user_snippet.user_id = '{user_id}'
+            SELECT urs.query, urs.reviewed_snippet
+            FROM user_reviewed_snippet urs
+            INNER JOIN user_snippet us 
+                on urs.snippet_id = us.snippet_id
+            WHERE us.user_id = '{user_id}'
         """
 
         example_list = self.cursor.fetch_dict(sql_query)
         
-        if len(example_list) >= 5:
+        if len(example_list) >= 3:
             # use the user-examples as the only ones for the final prompt
             self.prompt = ""
 
         for example in example_list:
             prompt_append = f"\nQuery: {example['query']}"
-            prompt_append += '\nResult:'
             prompt_append += f"\n{example['generated_snippet']}"
             
             self.prompt.append(prompt_append)
@@ -220,7 +243,111 @@ class QueryPipeline:
 
         # check if choices returned, return top choice
         if 'choices' not in res_dict or len(res_dict['choices']) == 0:
-            return None
+            return None, None, 'Model returned empty results.'
         
-        return res_dict['choices'][0]['text']
+        result = res_dict['choices'][0]['text']
+        
+        # save result to databse
+        query = f"""
+        INSERT INTO snippet (query, generated_snippet)
+        VALUES ('{self.current_user_prompt}', '{result}')
+        RETURNING snippet_id
+        """
+        
+        res = self.cursor.fetch_dict(query)
+
+        if len(res) == 0:
+            # failed to write to database
+            return result, None, 'Failed to save snippet in database'
+
+        self.cursor.commit()
+
+        return result, res[0]['snippet_id'], ''
+    
+    def insert_user_reviewed_snippet(self, input):
+        """
+            Public method to insert user reviewed snippet.
+
+            Input Dict Properties
+            ---------------------
+            user_id : {str}
+                String identifier of user
+            
+            user_query : {str}
+                String representation of the user's original query
+
+            snippet_id : {int}
+                Identifier of original snippet (model generated).
+
+            reviewed_snippet : {str}
+                String representation of reviewed snippet
+        """
+        if not isinstance(input, dict) or len(input) == 0:
+            return False
+
+        user_id = input['user_id']
+        user_query = input['user_query']
+        original_snippet_id = input['snippet_id']
+        reviewed_snippet = input['reviewed_snippet']
+
+        # create a record in the user table
+        query = f"""
+        INSERT INTO ss_user (user_id)
+        VALUES ('{user_id}')
+        RETURNING user_id
+        """
+
+        res = self.cursor.fetch_dict(query)
+
+        if len(res) == 0:
+            # failed to insert user into user table
+            return False, 'Failed to insert user into user table.'
+        
+        user_id = res[0]['user_id']
+
+        # create a record for the reviewed snippet
+        query = f"""
+        INSERT INTO user_reviewed_snippet (query, reviewed_snippet)
+        VALUES ('{user_query}', '{reviewed_snippet}')
+        RETURNING snippet_id
+        """
+
+        res = self.cursor.fetch_dict(query)
+
+        if len(res) == 0:
+            # failed to insert into user reviewed snippet table
+            return False, 'Failed to insert snippet into user review table.'
+
+        reviewed_snippet_id = res[0]['snippet_id']
+
+        # create mapping record in user snippet table
+        query = f"""
+        INSERT INTO user_snippet (snippet_id, user_id)
+        VALUES ({reviewed_snippet_id}, '{user_id}')
+        RETURNING snippet_id
+        """
+
+        res = self.cursor.fetch_dict(query)
+
+        if len(res) == 0:
+            # failed to insert map between user and reviewed snippet
+            return False, 'Failed to insert map record in user snippet table.'
+        
+        # create snippet map record
+        query = f"""
+        INSERT INTO model_review_snippet (model_snippet_id, reviewed_snippet_id)
+        VALUES ({original_snippet_id}, {reviewed_snippet_id})
+        RETURNING model_snippet_id
+        """
+
+        res = self.cursor.fetch_dict(query)
+
+        if len(res) == 0:
+            # failed to insert map between snippets
+            return False, 'Failed to insert map record in snippet-snippet table.'
+        
+        # commit insertion changes
+        self.cursor.commit()
+
+        return True, ''
     
--- table-create.sql
+++ table-create.sql
@@ -1,16 +1,34 @@
 -- entity tables
+
+-- model generated snippet table
 CREATE TABLE snippet (
     snippet_id SERIAL PRIMARY KEY,
     query TEXT,
     generated_snippet TEXT
 );
 
+-- user reviewed snippet table
+CREATE TABLE user_reviewed_snippet (
+    snippet_id SERIAL PRIMARY KEY,
+    query TEXT,
+    reviewed_snippet TEXT
+);
+
+-- user table
 CREATE TABLE ss_user (
     user_id TEXT PRIMARY KEY
 );
 
 -- relationship tables
+
+-- map between the user and their reviwed snippet
 CREATE TABLE user_snippet (
-    snippet_id SERIAL REFERENCES snippet (snippet_id),
+    snippet_id SERIAL REFERENCES user_reviewed_snippet (snippet_id),
     user_id TEXT REFERENCES ss_user (user_id)
+);
+
+-- map between model generated and user reviewed snippets
+CREATE TABLE model_review_snippet (
+    model_snippet_id SERIAL REFERENCES snippet (snippet_id),
+    reviewed_snippet_id SERIAL REFERENCES user_reviewed_snippet (snippet_id)
 );
