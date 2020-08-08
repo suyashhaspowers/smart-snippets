e64d492ce8f8d4f0f62a9254cdc4f8f0bd7a5021
Siddhanth Unnithan <sidunnithan@gmail.com>
2020-08-08 01:50:37 -0400

    Testing different starting prompts and adding more comprehensive test cases

--- server/src/default_prompt.py
+++ server/src/default_prompt.py
@@ -1,13 +1,52 @@
+# primary training example
 default_prompt = """Query: A list from 1 to 10 with only even numbers.
-Result:
 l = [num for num in range(1,11) if num % 2 == 0]
 
 Query: A dict with cat and dog keys and examples as values.
-Result:
 d = {'cat': ['persian', 'siamese'], 'dog': ['labrador', 'poodle']}
 
 Query: A loop that goes from 1 to 10 and prints out hello each time.
-Result:
 for i in range(1,11):
 \tprint('hello')
 """
+
+# basic data types
+basic_data_types_prompt = """Query: A string
+s = 'abcd'
+
+Query: A list
+l = [1,2]
+
+Query: A set
+s = set([1,2,3])
+
+Query: A class
+class Foo:
+\tdef __init__(self):
+\t\tprint("class")
+
+Query: A dict
+d = {'x': 'y'}
+"""
+
+basic_control_flow_prompt = """Query: An if/else block
+if x:
+\tprint("if")
+else:
+\tprint("else")
+
+Query: A for loop
+for idx in range(x, y):
+\tprint("for loop")
+
+Query: A while loop
+x = 0
+while (x < y):
+\tprint("while")
+
+Query: A function
+def func(x,y):
+\treturn
+"""
+
+data_type_control_flow_prompt = basic_data_types_prompt + "\n" + basic_control_flow_prompt
--- test-service.sh
+++ test-service.sh
@@ -1,2 +1,25 @@
 # hit submit query endpoint and pass user prompt
-curl -X POST -H "Content-Type: application/json" --data '{"user_id": "3ajasd343", "query": "A loop from 1 to 5 that prints out hello for every other iteration"}' http://localhost:5000/submit_query
+echo "Query: A loop from 1 to 5 that prints out hello for every other iteration"
+curl --data '{"user_id": "3ajasd343", "query": "A loop from 1 to 5 that prints out hello for every other iteration"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+# be specific about the type of loop that we want
+echo "Query: A while loop that prints test 5 times"
+curl --data '{"user_id": "3ajasd343", "query": "A while loop that prints test every other iteration 5 times"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+# basic data structures
+echo "Query: A set with 3 string items"
+curl --data '{"user_id": "3ajasd343", "query": "A set with 3 string items"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+echo "Query: A list with 3 integers"
+curl --data '{"user_id": "3ajasd343", "query": "A list with 3 integers"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+echo "Query: A nested dict with 2 keys"
+curl --data '{"user_id": "3ajasd343", "query": "A nested dict with 2 keys"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+# class snippets
+echo "Query: A Pet class with a function called adopt"
+curl --data '{"user_id": "3ajasd343", "query": "A Pet class with a function called adopt"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
+
+# test context outside of Python
+echo "Query: A list with 3 fruits"
+curl --data '{"user_id": "3ajasd343", "query": "A list with 3 fruits"}' -X POST -H "Content-Type: application/json" http://localhost:5000/submit_query
